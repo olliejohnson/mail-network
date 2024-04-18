bb76413503f8b3c253e47e5c8176003a8902079a
olliejohnson <oliver.joseph.johnson@gmail.com>
2024-04-18 10:10:20 -0400

    Added to ui

--- client/ui.lua
+++ client/ui.lua
@@ -2,15 +2,32 @@ local basalt = require("basalt")
 
 local main = basalt.createFrame()
 
-local button = main:addButton()
-button:setPosition(4,4)
-button:setSize(16,3)
-button:setText("Click me!")
+local messages_list = main:addFrame():setScrollable():setSize("parent.w/2", "parent.h"):setPosition(0, 0)
+local list = messages_list:addList()
 
-local function buttonClick()
-    basalt.debug("I got clicked!")
+messages = {{id = 0,subject = "Important", sender = "jacoberrol", body = "hello", has_item = true, item_id = "fwofmwlk"}}
+frames = {}
+
+function getMessageFromSubject(subject)
+    for message in pairs(messages) do
+        if message.subject == subject then
+            return message
+        end
+    end
 end
 
-button:onClick(buttonClick)
+function onMessageSelect(self, event, item)
+    message = getMessageFromSubject(item.text)
+    messages_list:hide()
+    frames[message.id]:show()
+
+end
+
+function parse_message(message, list)
+    list:addItem(message.subject)
+    message = main:addFrame():setPosition(0, 0):setSize("{parent.w/2}", "{parent.h}"):hide()
+    message:addLabel():setText(message.body):setFontSize(2)
+    frames[message.id] = message
+end
 
 basalt.autoUpdate()
